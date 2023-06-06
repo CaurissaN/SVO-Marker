ca2e2f3c02e6cd70eff260cb2e3b6d4c6424c14d
CaurissaN <caurissanev@gmail.com>
2023-06-06 10:42:15 -0400

    slowly setting up objects to create based on inquirer input

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 const inquirer = require("inquirer");
-const {Shape, SGV, Circle, Triangle, Square} = require("./lib/shapes")
+const { writeFile } = require("fs/promises");
+const { Shape, SVG, Circle, Triangle, Square } = require("./lib/shapes");
 
+function init() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "shape",
+            message: "What shape would you like your SVG to be?",
+            choices: ["Circle", "Triangle", "Square"]
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "What background color would you like your SVG to be?"
+        },
+        {
+            type: "input",
+            name: "text",
+            message: "What text would you like to be inside your SVG?"
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "What text color would you like your SVG's text to be?"
+        }
+    ]).then((res) => {
+        let shape;
+        if (res.shape === "Circle") {
+            shape = new Circle()
+        } else if (res.shape === "Triangle") {
+            shape = new Triangle()
+        } else {
+            shape = new Square()
+        }
+        shape.setColor(res.shapeColor)
 
+        const svg = new SVG()
+        svg.setText(res.text, res.textColor)
+        svg.setShape(shape)
+        console.log(svg)
+    })
+}
+
+init()
